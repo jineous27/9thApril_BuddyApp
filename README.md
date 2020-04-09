df77b6ff61a0e6bd4ebd853ad110fbfdb9b94cc8
jinheeyi <jinhee.yi27@gmail.com>
2020-04-10 09:44:19 +1000

    Install Express library & Set up a server

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 9thApril_BuddyApp
+
+Second practice file to budil Buddy App 2020!
---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+
+
+
+
+
+
+
+
+
+
+
+const port =1111;
+app.listen(port, console.log(`Server is running on this port ${port}`));
