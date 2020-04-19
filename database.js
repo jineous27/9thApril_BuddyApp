0708fc5b2b41b353f575f2f1f6e9bb75374463a4
jinheeyi <jinhee.yi27@gmail.com>
2020-04-19 22:19:30 +1000

    Add basic code lines -

---
+++ database.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+mongoose.connect("", {useNewUrlParser:true})
+    .then() => console.log ("Congratulations! MongoDB is connected")
+    .catch(err => console.log(error));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
